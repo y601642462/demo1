2eed8675d65d7cf6eb3eadd7dc95a39c32496148
Wandell <601642462@qq.com>
2018-10-30 09:47:20 +0000

    Finish user edit,update,index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
     public function create()
     {
         return view('sessions.create');
@@ -19,9 +25,9 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials,\request()->has('remember'))) {
+        if (Auth::attempt($credentials,$request->has('remember'))) {
             session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show',[Auth::user()]));
         } else {
             session()->flash('danger','很抱歉，您的邮箱和密码不正确');
             return redirect()->back();
