4003c5c48fcaa15ff76ad1ac70bdea475a380d47
Wandell <601642462@qq.com>
2018-11-01 08:36:35 +0000

    Add password reset & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -94,13 +94,11 @@ class UsersController extends Controller
     {
         $view = "emails.confirm";
         $data = compact('user');
-        $from = '601642462@qq.com';
-        $name = 'Wandell';
         $to = $user->email;
         $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
 
-        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -9,6 +9,7 @@
             </div>
             <div class="panel-body">
                 @include('shared._errors')
+
                 <form method="post" action="{{route('login')}}">
                     {{csrf_field()}}
 
@@ -18,7 +19,7 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="password">密码：</label>
+                        <label for="password">密码（<a href="{{ route('password.request') }}">忘记密码</a>）：</label>
                         <input type="password" name="password" class="form-control" value="{{old('password')}}">
                     </div>
                     <div class="checkbox">
